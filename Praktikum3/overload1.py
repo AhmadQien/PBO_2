dd44706d00b2aae1cd74bac0509d01d6b012c50c
AhmadQien <126934105+AhmadQien@users.noreply.github.com>
2023-04-03 18:45:40 +0700

    Add files via upload

---
+++ Praktikum3/overload1.py
@@ -0,0 +1,3 @@
+print(34 + 77) # output: 111 
+print("Hello" + " My " + " Name " + " Is " + " Muhammad Al Muttaqien ") # output: Hello My Name Is Muhamamd Al Muttaqien
+print([2, 4] + [5, 7] + [24, 26, 30]) # output: [2, 4, 5, 7, 24, 26, 30]
---
+++ Praktikum3/overload2.py
@@ -0,0 +1,9 @@
+x = [109, 220, 34, 68, 77] 
+x.sort() 
+print(x) 
+# output: [34, 68, 77, 109, 220] 
+
+y = ["s", "a", "r", "c", "b"] 
+y.sort() 
+print(y) 
+# output: ['a', 'b', 'c', 'r', 's']
